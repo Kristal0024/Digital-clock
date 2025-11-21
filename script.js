9460618e025a1ed63dda6baa80d671b09218e65d
Kristal Shrestha <kristalstha20@gmail.com>
2025-11-21 14:52:37 +0545

    this is a digital clock

---
+++ script.js
@@ -0,0 +1,17 @@
+const timer=document.querySelector(".timer")
+const meridiems=document.querySelector('.meridiems')
+
+function showtime(){
+const now=new Date()
+let hour=now.getHours();
+const minutes=String(now.getMinutes()).padStart(2,0);
+hour=hour%12;
+if(hour===0){
+    hour=12;
+}
+timer.textContent=`${hour}:${minutes}`
+const ampm=now.getHours()>=12?"PM":"AM";
+meridiems.textContent=`${ampm}`
+}
+// showtime()
+setInterval(showtime,1000)

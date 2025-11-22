812d4544a39c36c494210ba3b55cefb114d7dd8b
Kristal Shrestha <kristalstha20@gmail.com>
2025-11-22 08:49:22 +0545

    seconds added and box shadow

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ function showtime(){
 const now=new Date()
 let hour=now.getHours();
 const minutes=String(now.getMinutes()).padStart(2,0);
+const seconds=String(now.getSeconds()).padStart(2,0);
 hour=hour%12;
 if(hour===0){
     hour=12;
 }
-timer.textContent=`${hour}:${minutes}`
+timer.textContent=`${hour}:${minutes}:${seconds}`
 const ampm=now.getHours()>=12?"PM":"AM";
 meridiems.textContent=`${ampm}`
 }
